4b6c4bd195b604e21cdf9d9268e1014538e912ee
Jakub Krej <44982163+JakubKrej@users.noreply.github.com>
2019-08-05 14:09:24 +0200

    'mp3-start'

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module mp3player {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports pl.java.mp3player.main to javafx.graphics;
+    opens pl.java.mp3player.controller to javafx.fxml;
+}
