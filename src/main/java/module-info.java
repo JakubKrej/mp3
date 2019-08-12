5d83f1386bd8c427e8d4607fd0ff2477d406f04e
jakubkrej <44982163+JakubKrej@users.noreply.github.com>
2019-08-12 23:09:34 +0200

    Add files via upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module mp3player {
         requires javafx.graphics;
         requires javafx.controls;
         requires javafx.fxml;
+        requires javafx.media;
+        requires jid3lib;
 
         exports pl.java.mp3player.main to javafx.graphics;
         opens pl.java.mp3player.controller to javafx.fxml;
+        opens pl.java.mp3player.mp3 to javafx.base;
         }
